b021a9a49f98f770be2c18808338057b443a4c2e
Izydor <izydor4678@gmail.com>
2026-01-14 21:49:41 +0100

    feat: add wasm build

--- game/src/plugins/defaults.rs
+++ game/src/plugins/defaults.rs
@@ -1,5 +1,4 @@
-use bevy::prelude::*;
-
+use bevy::{asset::AssetMetaCheck, prelude::*};
 // TODO: delete this variable
 const BACKGROUND_COLOR: Color = Color::srgb(0.4, 0.4, 0.4);
 
@@ -9,6 +8,10 @@ pub(crate) fn plugin(app: &mut App) {
     app.insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_plugins(
             DefaultPlugins
+                .set(AssetPlugin {
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                })
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         // TODO: create a config file that contains all these config properties
